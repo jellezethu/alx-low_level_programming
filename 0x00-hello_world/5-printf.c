29a85a79359b4ca40231872859dc7a2b3e10dbf0
Zethu <jellezethu@gmail.com>
2023-03-17 03:41:42 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,4 +1,4 @@
-#include<stdio.h>
+#include <stdio.h>
 
 /**
  * main - prints a c program using printf
@@ -8,6 +8,5 @@
 int main(void)
 {	
 	printf("with proper grammer, but the outcome is a piece of art,\n");
-
 	return (0);
 }

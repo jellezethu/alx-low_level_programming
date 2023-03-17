10aa959be40c988a5cd670bea0f8b933c3499b44
Zethu <jellezethu@gmail.com>
2023-03-16 22:50:08 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -4,6 +4,8 @@
  *
  * Return : 0
 */
-int main (void)
-{	printf("with proper grammer, but the outcome is a piece of art.")
+int main(void)
+{	
+	printf("with proper grammer, but the outcome is a piece of art.\n");
+	return (0);
 }

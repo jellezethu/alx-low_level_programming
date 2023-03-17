732fb77ce096a62e11f8b6ace3ad40ab715c972a
Zethu <jellezethu@gmail.com>
2023-03-16 22:34:22 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <studio.h>
+/*
+ * main - print a line code using puts
+ * Retrun: 0
+ */
+int main (void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}

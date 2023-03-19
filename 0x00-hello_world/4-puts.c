e191c257c9c7e9d4a161effcc020538cfa58fbc1
Zethu <jellezethu@gmail.com>
2023-03-19 04:03:06 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
 /*
- * main - print a line code using puts
- * Retrun: 0
+ * main - Entry point
+ *
+ * description - A program that prints a line with a puts function
+ *
+ * Retrun: 0 (success)
  */
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle"\n");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }

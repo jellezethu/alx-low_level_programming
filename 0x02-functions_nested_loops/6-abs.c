99d8398813af31f22a9f025ce3071ea78c6a9b8a
Zethu <jellezethu@gmail.com>
2023-03-21 01:47:27 -0700

    6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include <main.h>
+
+/**
+ * _abs -  prints absolute value of a number
+ * @i: number targeted
+ *
+ * Return: Always (0)
+ */
+	int _abs(int i)
+{
+	if (i > 0)
+		return (i);
+	else if (i < 0)
+		return (-i);
+	else
+		return (0);
+}

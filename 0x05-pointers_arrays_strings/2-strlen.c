bd3ff8cfa1fc36bbff54cd8f2689006061aff293
Zethu <jellezethu@gmail.com>
2023-03-27 05:55:07 -0700

    2-strlen.c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _sterlen - returns the value of a string
+ * @s - string whos length to return
+ * Return: the value of the string
+ *
+ */
+int _strlen(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+		i++;
+	return (i);
+}

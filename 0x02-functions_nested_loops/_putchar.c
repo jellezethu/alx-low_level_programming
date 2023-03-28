c4d0aa89366a0f4451bf90ea4114c939977167ad
Zethu <jellezethu@gmail.com>
2023-03-28 08:27:15 -0700

    11-print_to_98.c

--- 0x02-functions_nested_loops/_putchar.c
+++ 0x02-functions_nested_loops/_putchar.c
@@ -3,7 +3,7 @@
 /**
  * main - Entry point.
  * Description: write a program that prints "_putchar" followed by a new line.
- * Return: Always 0 (Success.)
+ * Return: nothing, return void
  */
 int main(void)
 {

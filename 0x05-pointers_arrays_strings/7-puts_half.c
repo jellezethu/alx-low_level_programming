610cb72f2d4ca88784980489e98d28b7056842f7
Zethu <jellezethu@gmail.com>
2023-03-28 03:29:59 -0700

    7-puts_half

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,35 @@
+#include "main.h"
+/**
+ * puts_half - Prints half of a string, followed by a new line
+ *
+ * @str: The string to print half of
+ *
+ * Return: nothing
+ */
+
+void puts_half(char *str)
+{
+	int len;
+	int j;
+	char newStr;
+
+	len = _strlen(str);
+
+	if (len % 2 ==  0)
+	{
+		for (j = len / 2; j < len; j++)
+		{
+			newStr = str[j];
+			_putchar(newStr);
+		}
+	}
+	else
+	{
+		for (j = (len + 1) / 2; j < len; j++)
+		{
+			newStr = str[j];
+			_putchar(newStr);
+		}
+	}
+	_putchar('\n');
+}
--- 0x05-pointers_arrays_strings/_putchar.c
+++ 0x05-pointers_arrays_strings/_putchar.c
@@ -11,3 +11,13 @@ int _putchar(char c)
 {
 	return (write(1, &c, 1));
 }
+
+	int _strlen(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+		i++;
+
+	return (i);
+}

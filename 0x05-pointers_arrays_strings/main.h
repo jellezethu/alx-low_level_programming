f65a5e65252447799098e72a2b82fa435112124c
Zethu <jellezethu@gmail.com>
2023-03-28 03:17:35 -0700

    8-print_array.c

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -6,6 +6,7 @@ void swap_int(int *a, int *b);
 int _strlen(char *s);
 void _puts(char *str);
 int _putchar(char c);
+void puts2(char *str);
 void print_rev(char *s);
 void puts_half(char *str);
 void print_array(int *a, int n);

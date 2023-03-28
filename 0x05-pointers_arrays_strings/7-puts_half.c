8563550f2e04b8264bac59180da61ef9499a7640
Zethu <jellezethu@gmail.com>
2023-03-28 05:00:33 -0700

    7-puts_half.c

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -33,3 +33,20 @@ void puts_half(char *str)
 	}
 	_putchar('\n');
 }
+/**
+ * _strlen - returns the length of a string
+ *
+ * @s: the string whose length to return
+ *
+ * Return: the length of the string
+ */
+
+int _strlen(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+		i++;
+
+	return (i);
+}

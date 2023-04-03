744b9e3c1c0cdd343daeee42ab9d41e4f3cd129b
Zethu <jellezethu@gmail.com>
2023-04-03 05:13:51 -0700

    0-memset.c

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -7,19 +7,14 @@
  *Return: dest
  */
 
-char *_memcpy(char *dest, char *src, unsigned int n)
-
+char *_memset(char *s, char b, unsigned int n)
 {
 	unsigned int i;
 
-	i = 0;
-	while (i < n)
+	for (i = 0; i < n; i++)
 	{
-		*(dest + i) = *(src + i);
-		i++;
-
+		s[i] = b;
 	}
 
-
-	return (dest);
+	return (s);
 }
